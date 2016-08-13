06950db845f353b1ca65a3f5ce5c425a23d2e083
Ivan Seidel <ivanseidel@gmail.com>
2016-08-13 09:18:35 -0300

    Fixed settings file path

--- config/env/development.js
+++ config/env/development.js
@@ -3,6 +3,6 @@ var path = require('path');
 module.exports = {
   name: 'Tournamenter Manager',
   icon: path.join(__dirname, '../../', 'midia/Tournamenter_appicon.png'),
-  settings_file: path.join(__dirname, '../../.settings.json'),
+  settings_file: path.join(electron.app.getPath('userData'), '.settings.json'),
   fileModelsDir: 'FileModels',
 };
